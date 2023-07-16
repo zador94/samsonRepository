59a140634d7f9db41e7fe7f68b86caae55be702a
azaza <zador94@mail.ru>
2023-07-16 13:26:51 +0300

    2.php commit two task

--- 2.php
+++ 2.php
@@ -18,3 +18,38 @@ $str = 'Lorem amet dolor sit amet, consectetur amet elit. Omnis, rem!';
 $podstr = 'amet';
 echo convertString($str, $podstr);
 
+/*----------------------------------------------------------------------------------------*/
+
+function mySortForKey($a, $b) {
+
+    for ($i = 0; $i < count($a); $i++) {
+        if (!isset($a[$i][$b])) {
+            throw new Exception("Ошибка: отсутствует ключ {$b} в одном из вложенных массивов");
+        }
+    }
+
+    for ($i = 0; $i < count($a) - 1; $i++) {
+        for ($j = 0; $j < count($a) - $i - 1; $j++) {
+            if ($a[$j][$b] > $a[$j + 1][$b]) {
+                $temp = $a[$j];
+                $a[$j] = $a[$j + 1];
+                $a[$j + 1] = $temp;
+            }
+        }
+    }
+
+    return $a;
+}
+
+$a = [
+    ['a' => 2, 'b' => 1],
+    ['a' => 1, 'b' => 3]
+];
+$b = 'a';
+
+try {
+    $result = mySortForKey($a, $b);
+    var_dump($result);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
